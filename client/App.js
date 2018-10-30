b2afd81dfcd2431b262fe322cb72235e8aec552a
Morteza Taghdisi <morteza.taghdisi@yahoo.com>
2018-10-30 15:13:18 +0330

    server added

--- client/App.js
+++ client/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Camera from "react-native-camera";
 export default class App extends Component {
+
+
+
   render() {
     return (
-      <Camera style={{ flex: 1 }}
-        ref={cam => this.camera = cam}
-        aspect={Camera.constants.Aspect.fill}>
-
-      </Camera>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, height: 50}}>
+        </View>
+        <Camera style={{ flex: 1 }}
+          ref={cam => this.camera = cam}
+          aspect={Camera.constants.Aspect.fill}>
+        </Camera>
+      </View>
     );
   }
 }
